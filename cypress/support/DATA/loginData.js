a862cf8f64099c62e4dadba06e3c81d07878cbfd
unknown <hernan97carp@hotmail.com>
2024-02-06 19:18:36 -0300

    resolve conflicts

--- cypress/support/DATA/loginData.js
+++ cypress/support/DATA/loginData.js
@@ -1,7 +1,6 @@
 const dashboardIndex = Cypress.env('endpoint').dashboardIndex;
 
 const adminUserData = {
-<<<<<<< HEAD
 	username: Cypress.env('AdminUser').username,
 	password: Cypress.env('AdminUser').password,
 	usernameEmpty: Cypress.env('AdminUser').usernameEmpty,
@@ -14,17 +13,3 @@ module.exports = {
 	dashboardIndex,
 	adminUserData,
 };
-=======
-  username: Cypress.env('AdminUser').username,
-  password: Cypress.env('AdminUser').password,
-  usernameEmpty: Cypress.env('AdminUser').usernameEmpty,
-  passwordEmpty: Cypress.env('AdminUser').passwordEmpty,
-  invalidUsername: Cypress.env('AdminUser').invalidUsername,
-  invalidPassword: Cypress.env('AdminUser').invalidPassword,
-};
-
-module.exports = {
-  dashboardIndex,
-  adminUserData,
-};
->>>>>>> 94edb56a786dd1055340aa41d910c0b9ba484a15
--- cypress/support/POM/Login.Page.js
+++ cypress/support/POM/Login.Page.js
@@ -1,12 +1,24 @@
 class Login {
-  get = {
-    usernameInput: () => cy.get('[name="username"]'),
-    passwordInput: () => cy.get('[name="password"]'),
-    submitButton: () => cy.get('[type="submit"]'),
-    forgotLink: () => cy.get('[class*="login-forgot"] p'),
-    emptyInputUsername: () => cy.get(":nth-child(2) > .oxd-input-group > .oxd-text"),
-    emptyInputPassword: () => cy.get(":nth-child(3) > .oxd-input-group > .oxd-text"),
-  };
+	get = {
+		usernameInput: () => cy.get('[name="username"]'),
+		passwordInput: () => cy.get('[name="password"]'),
+		submitButton: () => cy.get('[type="submit"]'),
+		forgotLink: () => cy.get('[class*="login-forgot"] p'),
+		emptyInputUsername: () => cy.get(':nth-child(2) > .oxd-input-group > .oxd-text'),
+		emptyInputPassword: () => cy.get(':nth-child(3) > .oxd-input-group > .oxd-text'),
+		invalidCredentials: () => cy.get('p.oxd-text.oxd-text--p.oxd-alert-content-text'),
+		
+	};
+	dropdown ={
+		userOptionsDropdown:() =>cy.get('.oxd-userdropdown-tab'),
+		userOptionDropdownLogOut: ()=> cy.get(':nth-child(4) > .oxd-userdropdown-link')
+	}
+
+  dropdownHeaderHamburger = {
+	menuDropdown: ()=>  cy.get('.oxd-main-menu-search > .oxd-icon-button'),
+	itemAdmin: () =>  cy.get('a[href="/web/index.php/admin/viewAdminModule"]'),
+	itemDashboard: () => cy.get(':nth-child(8) > .oxd-main-menu-item')
+  }
 
 	enterUsername(type) {
 		if (type) {
@@ -20,9 +32,34 @@ class Login {
 		}
 	}
 
-  submitLogin() {
-    this.get.submitButton().click();
-  }
+	submitLogin() {
+		this.get.submitButton().click();
+	}
+
+	errorMessageInvalidCredentials() {
+		this.get.invalidCredentials().should('exist').should('have.text', 'Invalid credentials');
+	}
+
+	emptyInputUsernameRequired() {
+		this.get
+			.emptyInputUsername()
+			.should('exist')
+			.should('be.visible')
+			.and('have.text', 'Required');
+	}
+	emptyInputPasswordRequired() {
+		this.get
+			.emptyInputPassword()
+			.should('exist')
+			.should('be.visible')
+			.and('have.text', 'Required');
+	}
+	dropdownLogOut() {
+	this.dropdown.userOptionsDropdown().should('be.visible').click()
+		  
+   this.dropdown.userOptionDropdownLogOut().click();
+		
+	  }
 }
 
 export const login = new Login();
